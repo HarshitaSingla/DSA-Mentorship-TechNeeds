8f4ea626e18b8f6430c552a773286980ce118a42
HarshitaSingla <119716371+HarshitaSingla@users.noreply.github.com>
2023-10-07 19:59:27 +0530

    I have done question 1 of assignment1.

---
+++ a1_q1.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int i;
+    for (i = 1; i <= 5; i++)
+    {
+        cout << "*";
+    }
+    cout << "\n";
+    for (i = 1; i <= 3; i++)
+    {
+        cout << " ";
+    }
+    cout << "*"
+         << "\n";
+    for (i = 1; i <= 2; i++)
+    {
+        cout << " ";
+    }
+    cout << "*"
+         << "\n";
+    for (i = 1; i <= 1; i++)
+    {
+        cout << " ";
+    }
+    cout << "*"
+         << "\n";
+    for (i = 1; i <= 5; i++)
+    {
+        cout << "*";
+    }
+
+    return 0;
+}
